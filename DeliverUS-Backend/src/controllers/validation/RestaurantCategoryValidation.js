1aa721b912a4c79aefdb177665408cfc55ec6ae2
RARES_NICOLAE PETRE <rarpet@alum.us.es>
2025-05-14 00:57:37 +0200

    backend

---
+++ DeliverUS-Backend/src/controllers/validation/RestaurantCategoryValidation.js
@@ -0,0 +1,22 @@
+import { RestaurantCategory } from '../../models/models.js'
+import { check } from 'express-validator'
+
+const checkAlreadyExistCategory = async (value, { req }) => {
+  try {
+    const restCategory = await RestaurantCategory.findOne({ where: { name: value } })
+    if (restCategory !== null) {
+      return Promise.reject(new Error(`The category ${value.name} already exists.`))
+    } else {
+      return Promise.resolve()
+    }
+  } catch (err) {
+    return Promise.reject(new Error(err))
+  }
+}
+
+const create = [
+  check('name').exists().isString().isLength({ min: 1, max: 50 }).trim(),
+  check('name').custom(checkAlreadyExistCategory)
+]
+
+export default { create }
